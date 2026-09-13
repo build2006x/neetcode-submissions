8872d80df6e65454e5e3400c7aa884b476ce56df
barath kumar <barathkunarma.cse2024@citchennai.net>
2026-09-13 20:38:12 +0530

    Add: squares-of-a-sorted-array - submission-0

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+         
+        pointer = 0
+    
+
+        while pointer < len(nums):
+              nums[pointer] = nums[pointer] ** 2
+              pointer +=1
+
+        return sorted(nums)          
