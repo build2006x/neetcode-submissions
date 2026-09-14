0fcf41111d1e2e798652e6be9be23c68d92ab18f
barath kumar <barathkunarma.cse2024@citchennai.net>
2026-09-14 10:43:30 +0530

    Add: assign-cookies - submission-37

---
+++ Data Structures & Algorithms/assign-cookies/submission-37.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        
+        g.sort()
+        s.sort()
+        
+        child = 0
+        cookie = 0
+        
+        while child < len(g) and cookie < len(s):
+            if s[cookie] >= g[child]:
+                child += 1  
+            cookie += 1 
+
+        ### why we sort means least greedy with least cookie can be orderly assign and check child can load with content 
+        
+        return child
+        
+
+             
